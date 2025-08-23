780e28baca15d8480a765623fed64bfaf4625973
Auronex <data0@rommesmo.no>
2025-08-23 10:37:20 +0200

    Removed rementants of earlier setup mistakes, altered basics, started setup of fronend changes. main is not fixed

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,44 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+import type { ThemeDefinition } from 'vuetify'
+
+// light theme
+const LightThemeC: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  }
+}
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'LightThemeC',
+    themes: {
+      LightThemeC,
+    },
+  },
+})
+
+// Export types for TypeScript support
+export type Vuetify = ReturnType<typeof createVuetify>
